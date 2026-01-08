d44c9c9759b8091b425701271f78cc89f67e565b
Dipanjan-Pradhan <pradhanpradhan@gmail.com>
2026-01-08 23:50:17 +0530

    union of two sorted array

---
+++ union_of_two_sorted_array.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> sortedArray(vector<int> a, vector<int> b){
+    int n1 = a.size();
+    int n2 = b.size();
+    int i = 0, j = 0;
+    vector <int> unionArray;
+    while(i < n1 && j < n2){
+        if(a[i] <= b[j]){
+            if(unionArray.size() == 0 || unionArray.back() != a[i])
+                unionArray.push_back(a[i]);
+            i++;
+        }
+        else{
+            if(unionArray.size() == 0 || unionArray.back() != b[j])
+                unionArray.push_back(b[j]);
+            j++;
+        }
+    }
+}
