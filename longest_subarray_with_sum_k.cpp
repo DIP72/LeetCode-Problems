c84628518d77dfc2a98e9b73a815a99d0159ec06
DIP72 <193588135+DIP72@users.noreply.github.com>
2026-05-06 14:46:24 +0530

    add with negetive numbers code

--- longest_subarray_with_sum_k.cpp
+++ longest_subarray_with_sum_k.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 
+
+// without negetive numbers
 int longestSubarrayWithSumK(vector<int> a, long long k) {
     long long sum = 0;
     int maxLen = INT_MIN;
@@ -17,5 +19,37 @@ int longestSubarrayWithSumK(vector<int> a, long long k) {
         }
         r++;
     }
+    return maxLen;
+}
+
+
+// with negetive numbers
+#include <bits/stdc++.h>
+using namespace std;
+
+int getLongestSubarray(vector<int>& nums, int k) {
+    unordered_map<long long, int> mp;
+    long long sum = 0;
+    int maxLen = 0;
+
+    for (int i = 0; i < nums.size(); i++) {
+        sum += nums[i];
+
+        // Case 1: subarray from 0 to i
+        if (sum == k) {
+            maxLen = i + 1;
+        }
+
+        // Case 2: check if (sum - k) seen before
+        if (mp.find(sum - k) != mp.end()) {
+            maxLen = max(maxLen, i - mp[sum - k]);
+        }
+
+        // Store only first occurrence
+        if (mp.find(sum) == mp.end()) {
+            mp[sum] = i;
+        }
+    }
+
     return maxLen;
 }
