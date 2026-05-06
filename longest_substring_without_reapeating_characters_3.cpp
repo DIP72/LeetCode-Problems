a5a2867b787ecb3d7a0b7069f3eca076610745e2
DIP72 <193588135+DIP72@users.noreply.github.com>
2026-05-06 13:00:03 +0530

    longest substring without reapeating characters leetcode -3

---
+++ longest_substring_without_reapeating_characters_3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int maxLen = INT_MIN;
+        int l = 0;
+        int r = 0;
+        int n = s.size();
+        if(n == 0) return 0;
+
+        vector<int> hash(255, -1);
+
+        while(r < n){
+            if(hash[s[r]] != -1 && hash[s[r]] >= l)
+                l = hash[s[r]] + 1;
+            int len = r - l + 1;
+            maxLen = max(len, maxLen);
+            hash[s[r]] = r;
+            r++;
+        }
+        return maxLen;
+    }
+};
