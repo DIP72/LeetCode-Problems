49f66b757329f0ecb3214d634fa7d177d2912aa3
DIP72 <193588135+DIP72@users.noreply.github.com>
2026-06-04 09:56:46 +0530

    minimum number with all set bits

---
+++ smallest_no_with_all_set_bits_3370.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int smallestNumber(int n) {
+        for(int i = 0; i < 32; i++){
+            if(n < (1 << i)) return (1 << i) - 1;
+        }
+        return n;
+    }
+};
