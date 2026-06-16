78dd2adeffdf225f92d710ae2c0b89923ce8bb93
DIP72 <193588135+DIP72@users.noreply.github.com>
2026-06-16 14:05:59 +0530

    single number III

---
+++ single_number_III_260.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        long xorr = 0;
+        for(int i = 0; i < n; i++)
+            xorr = xorr ^ nums[i];
+
+        long rightmost = (xorr & xorr-1) ^ xorr;
+        int b1 = 0;
+        int b2 = 0;
+        for(int i = 0; i < n; i++){
+            if(rightmost & nums[i]) b1 ^= nums[i];
+            else b2 ^= nums[i];
+        }
+        return {b1, b2};
+    }
+};
