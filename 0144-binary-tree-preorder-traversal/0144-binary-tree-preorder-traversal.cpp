5f2641d94d18a03e06a7a1d35e0a3276e34c86fe
Dipanjan Pradhan <193588135+DIP72@users.noreply.github.com>
2026-07-05 00:27:27 +0530

    Time: 2 ms (4.66%), Space: 11 MB (15.01%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -13,7 +13,7 @@ class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
         if(root == nullptr) return {};
-        vector<int> ans;
+        vector<int>ans;
         stack<TreeNode*> st;
         st.push(root);
         while(!st.empty()){
@@ -22,7 +22,7 @@ public:
 
             ans.push_back(temp->val);
             if(temp->right) st.push(temp->right);
-            if(temp->left) st.push(temp->left);            
+            if(temp->left) st.push(temp->left);
         }
         return ans;
     }
