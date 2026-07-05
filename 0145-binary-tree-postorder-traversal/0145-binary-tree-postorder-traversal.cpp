73a33458467336bfab9340f5aff4a807a260dbe2
Dipanjan Pradhan <193588135+DIP72@users.noreply.github.com>
2026-07-05 21:52:20 +0530

    Time: 0 ms (100%), Space: 10.9 MB (73.19%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -9,6 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+//      using two stack
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
