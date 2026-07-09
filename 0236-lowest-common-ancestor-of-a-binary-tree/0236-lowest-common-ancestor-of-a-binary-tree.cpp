099155721df78a9588852878958d0d8f15397bd6
Dipanjan Pradhan <193588135+DIP72@users.noreply.github.com>
2026-07-09 14:10:57 +0530

    Time: 59 ms (54.18%), Space: 43.9 MB (50.78%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,14 +10,16 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root == nullptr || root == p || root == q)
+        if (!root || root == p || root == q)
+            return root;
+        
+        TreeNode* lca_from_left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* lca_from_right = lowestCommonAncestor(root->right, p, q);
+        if (lca_from_left && lca_from_right)
             return root;
 
-        TreeNode *left = lowestCommonAncestor(root->left, p, q);
-        TreeNode *right = lowestCommonAncestor(root->right, p, q);
-
-        if(left == nullptr) return right;
-        else if(right == nullptr) return left;
-        else return root;
+        return lca_from_left ? lca_from_left : lca_from_right;
     }
 };
+
+// 看別人的 好聰明！
