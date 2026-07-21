c92ba974385d5cf55f610cfb2a6cf0bef443189b
Dipanjan Pradhan <193588135+DIP72@users.noreply.github.com>
2026-07-22 02:00:04 +0530

    Time: 0 ms (100%), Space: 22.8 MB (98.34%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -18,10 +18,11 @@ public:
         int rh = dfsHeight(root->right);
         if(rh == -1) return -1;
         if(abs(lh - rh) > 1) return -1;
+
         return 1 + max(lh, rh);
     }
 
     bool isBalanced(TreeNode* root) {
-        return dfsHeight(root) != -1;    
+        return dfsHeight(root) != -1;
     }
 };
